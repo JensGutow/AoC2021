7c75b0f15690a402801bc3fee9e4c946c39f079f
Jens Gutow <jens_gutow@web.de>
2021-12-28 14:29:40 +0100

    Nutze __hash__ und __eq__

--- AoC2021/Tag_22.py
+++ AoC2021/Tag_22.py
@@ -12,6 +12,12 @@ class Cube:
 
       self.coords = [(xmin, xmax), (ymin, ymax) ,(zmin, zmax)]
 
+  def __hash__(self):
+    return hash(tuple(self.coords))
+  
+  def __eq__(self, other):
+    return self.__hash__() == other.__hash__()
+
   def intersection(self, other):
     test = [ (max(item[0]) < min(item[1])) or (min(item[0]) > max(item[1])) for item in  list(zip(self.coords, other.coords))]
     has_intersection = not any(test)
@@ -38,6 +44,8 @@ class Cube:
   def __repr__(self):
     return self.__str__()
 
+  
+
 def parse_cube(line:str, limitCoords):
     swtich, line = line.split(" ")
     coords = [int(i) for i in re.findall("-?\d+", line)]
@@ -56,53 +64,40 @@ on x=10..12,y=10..12,z=10..12
 off x=9..11,y=9..11,z=9..11
 '''
 
-def getTotalVolume(OnCubes,  R):
+def getTotalVolume(OnCubes):
   v = 0
   for switch_cube, n in OnCubes.items():
-    v += n * R[switch_cube].getVolume()
+    v += n * switch_cube.getVolume()
   return v
 
 def solve(switches_and_cubes):
-
   OnCubes = Counter()
-  # es wurde "leider" eine eigene Klasse Cube erstellt.
-  # verschiedene Objekte mit gleichen Koordinaten sind verschiedene Objekte - und werden im Counter damit (exponentiell) mehrfach gezählt.
-  # daher wird als Schlüssel nicht das Objekt - sonder ein Hash - die Ausgabe von __str__()  - verwendet.
-  # die Regestry R dient als  "*Übersetzer" vom Hash zum Cube-Objekt
-  R = defaultdict()  # hash -> Cube (hash:= Cube.__str__())
-
+  
   for switch_cube in switches_and_cubes:
     #print("\ncurrent cube:(swtich, coords, V)", switch_cube, switch_cube[1].getVolume())
     current_switch, current_cube = switch_cube
-        
+ 
     # ermittle alle Durchschnitte aller bisher bekannten "On"-Wurfel mit dem aktuellen Cube der Eingabe
     # A = ist ein "ON" Cube aus dem Directory OnCubes (iterator), B ist aktueller Cube aus Input
     # V(A + B)  = V(A) + V(B) - V(A intersecion B) - relevant, wenn B ist ein ON-cube
     # V(A - B)  = V(A)        - V(A intersecion B) - relevant, wenn B ist in OFF-cube
     onCubes_temp = Counter()
     for onCube in OnCubes:
-      onCube = R[onCube]
       intersection = onCube.intersection(current_cube)
       if not intersection: continue
-      assert intersection.__str__() == current_cube.intersection(onCube).__str__()
-      onCubes_temp[intersection.__str__()] -= OnCubes[onCube.__str__()]
-      R[intersection.__str__()] = intersection
+      assert intersection == current_cube.intersection(onCube)
+      onCubes_temp[intersection] -= OnCubes[onCube]
       #print("Intersection found with:", onCube)
       #print("Intersection=", intersection, " V=", intersection.getVolume(), " V_total:", getTotalVolume(OnCubes,  R))
 
     # wenn aktueller Cube vom Type "on" -> Zaehle dieses Volumen EINMAL dazu
     if current_switch:
-        onCubes_temp[current_cube.__str__()] += 1
-        R[current_cube.__str__()] = current_cube
+        onCubes_temp[current_cube] += 1
         #print("add curent cube to dict, V:", current_cube.getVolume(), " V_total:", getTotalVolume(OnCubes,  R))
 
     for c in onCubes_temp:
-        OnCubes[c.__str__()] += onCubes_temp[c.__str__()]
-        R[current_cube.__str__()] = current_cube
-
-
-
-  return getTotalVolume(OnCubes, R)
+        OnCubes[c] += onCubes_temp[c]
+  return getTotalVolume(OnCubes)
   
 def test():
   coords_intersection = [5,9,-1,9,-12,3]
@@ -130,14 +125,10 @@ def test():
     ]
   c_no_i = [Cube(*c) for c in is_bigger]
   for c in c_no_i:
-        assert(c.intersection(c_i).__str__()  == c_i.__str__())
+        assert(c.intersection(c_i)  == c_i)
 
-  is_lower = [
 
-  ]
   
-
-      
 
 cubes = read_puzzle('Tag_22.txt', True)
 print("Part1:", solve(cubes))
@@ -146,12 +137,3 @@ cubes = read_puzzle('Tag_22.txt', False)
 print("Parat2:",solve(cubes))
 
 #test()
-
-
-# for cube in cubes:
-#   print(cube[0], cube[1])
-
-
-
-
-
