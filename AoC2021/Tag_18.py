04c2a8e9a3ca80a87a2330ec4a6aeefa18e492e8
Jens Gutow <jens_gutow@web.de>
2021-12-28 09:17:49 +0100

    Tag 22

--- AoC2021/Tag_18.py
+++ AoC2021/Tag_18.py
@@ -7,6 +7,7 @@ def read_puzzle(datei):
 
 
 class Number():
+    debug = False
     def __init__(self, initList : list, parent, c=None) -> None:
         self.c = self.left = self.right = self.parent = None
         if c != None:
@@ -27,29 +28,34 @@ class Number():
             self = self.parent
         return self
 
-    def GetSeals(self, node, seals):
-        if node.left: 
-            self.GetSeals(node.left, seals)
-        if node.c != None: 
-            seals.append(node)
-        if node.right: 
-            self.GetSeals(node.right, seals)
+    def GetPrecSucc2_worker(self,node, prec, succ, nodeFound = False):
+        if not node: return False, None, None, nodeFound
+        result = False
+        if self.left: 
+            result, prec, succ, nodeFound =  self.left.GetPrecSucc2_worker(node, prec, succ, nodeFound)
+            if result : return result, prec, succ, nodeFound
 
-    def GetPrecSucc(self, node):
-        root = self.GetRoot()
-        prec = succ =  None
-        nodeFound = False
-        seals = []
-        self.GetSeals(root, seals)
-        for seal in seals:
-            if seal not in [node.left, node.right]:
-                if not nodeFound:
-                    prec = seal
+        if self == node: 
+            nodeFound = True
+        else:
+            if (not self in (node, node.left,node.right)) and (self.c != None):
+                if nodeFound:
+                    succ = self
+                    result = True
+                    return  result, prec, succ, nodeFound
                 else:
-                    succ = seal
-                    break
-            else: 
-                nodeFound = True
+                    prec = self
+
+        if self.right: 
+            result, prec, succ, nodeFound = self.right.GetPrecSucc2_worker(node,prec, succ, nodeFound)
+        
+        return result, prec, succ, nodeFound
+
+
+    def GetAtomicPecSucc2(self, node):
+        root = self.GetRoot()
+        succ = prec = None
+        _, prec, succ, _ =  root.GetPrecSucc2_worker(node, prec, succ)
         return prec, succ
                 
     def __repr__(self) -> str:
@@ -58,7 +64,7 @@ class Number():
         right =self.right.__repr__()
         return "[" + left + "," + right + "]"
 
-    def clear_child(self, number):
+    def clear_child(self):
         self.right = self.left = None
         self.c = 0        
 
@@ -67,41 +73,54 @@ class Number():
 
     def split(self) -> bool:
         result = False
-        if self.c != None:
-            if self.c >= 10:
-                a = self.c//2
-                b = self.c-a
-                self.left = Number("", self, a)
-                self.right = Number("", self, b)
-                self.c = None
-                result =  True
-        else:
+        if self.left:
             result = self.left.split()
-            if not result:
-                result = self.right.split()
+        if not result:
+            if self.c != None:
+                if self.c >= 10:
+                    a = self.c//2
+                    b = self.c - a
+                    self.left = Number("", self, a)
+                    self.right = Number("", self, b)
+                    self.c = None
+                    result =  True
+            else:
+                if self.right:
+                    result = self.right.split()
         return result
 
     def explode(self, depth ) -> bool:
         if self.c != None: return False  # its only a int
         
         exploded = False
-        if self.is_atomic() and depth > 4:
-            prec, succ = self.GetPrecSucc(self)
-            if prec: prec.c += self.left.c
-            if succ: succ.c += self.right.c
-            self.clear_child(self)
-            exploded =  True
-        else:
-            exploded =  self.left.explode( depth+1)
-            if not exploded:
-                exploded = self.right.explode(depth+1)
+        if self.left:
+            exploded = self.left.explode(depth+1)
+        if not exploded:
+            if self.is_atomic() and depth > 4:           
+                prec = succ = None
+                prec, succ = self.GetAtomicPecSucc2(self)
+                if prec: prec.c += self.left.c
+                if succ: succ.c += self.right.c
+                succ = prec = 0
+                self.clear_child()
+                exploded =  True
+            else:
+                if self.right:
+                    exploded = self.right.explode(depth+1)
         return exploded
 
     def reduce(self):
         reduced = True
+        if Number.debug: print("REDUCE:", self)
         while(reduced):
-            if not self.explode(1):
+            exploded = self.explode(1)
+            if exploded and Number.debug:
+                print("After explode:", self)
+
+            if not exploded:
                 reduced = self.split()
+                if reduced and Number.debug:
+                    print("After split  :", self)
                 
     def add(self, other):
         s = Number([0,0],None)
@@ -146,12 +165,121 @@ def solve(puzzle):
         print("+ ", nr)
         n = n.add(nr)
         print("= ", n,"\n")
+ 
+def test():
+    explodeTestVector = [
+        [
+            [[[[[9,8],1],2],3],4],
+             [[[[0,9],2],3],4]
+        ],
+        [
+            [7,[6,[5,[4,[3,2]]]]] ,
+            [7,[6,[5,[7,0]]]] 
+        ],
+        [
+            [[6,[5,[4,[3,2]]]],1],
+            [[6,[5,[7,0]]],3]
+        ],
+        [
+            [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]],
+            [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]] 
+        ],
+        [
+            [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]],
+            [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
+        ],
+        [
+            [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]],
+            [[3,[2,[8,0]]],[9,[5,[7,0]]]]
+        ]
+    ]
+    print("EXploding Tests")
+    for test in explodeTestVector:
+        n1 = Number(test[0], None)    
+        n2 = Number(test[1], None)    
+        n1.explode(1)
+        assert n1.__repr__() == n2.__repr__()
+
+    splitTestVector = [ 
+        [
+            [[[[0,7],4],[15,[0,13]]],[1,1]], [[[[0,7],4],[[7,8],[0,13]]],[1,1]]
+        ],
+        [
+            [[[[0,7],4],[[7,8],[0,13]]],[1,1]], [[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]
+        ],
+    ]
+
+    print("Split Tests")
+    for test in splitTestVector:
+        n1 = Number(test[0], None)    
+        n2 = Number(test[1], None)    
+        n1.split()
+        assert n1.__repr__() == n2.__repr__()
+
+    addTestVector = [
+       [
+           [[1,1],[2,2],[3,3],[4,4]],    
+           [[[[1,1],[2,2]],[3,3]],[4,4]],
+           False
+       ],
+        [
+           [[1,1],[2,2],[3,3],[4,4],[5,5]],
+            [[[[3,0],[5,3]],[4,4]],[5,5]],
+            False
+       ],
+       [
+           [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6]],   
+         [[[[5,0],[7,4]],[5,5]],[6,6]],
+         False
+       ],
+       [ 
+           [ 
+                [[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]], 
+                [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+           ],
+           [[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]],
+           True
+       ]
+   ]
+
+    print("Test Add")
+    for i, test in enumerate(addTestVector):
+        Number.debug = test[2]
+        sum_setpoint = Number(test[1], None)
+        summanden = test[0]
+        sum = Number(summanden[0], None)
+        for summand in summanden[1:]:
+            sum = sum.add(Number(summand, None))
+        criteria = sum.__repr__() == sum_setpoint.__repr__()
+        print("test", i, criteria)
+        if not criteria:
+            print("sum setpoint:", sum_setpoint)
+            print("sum         :", sum)
+        #assert sum.__repr__() == sum_setpoint.__repr__()
+        
+
+
+
+
+
+
+# puzzle = read_puzzle('Tag_18_Bsp.txt')
+
+# solve(puzzle)
+test()
+
+t1 = Number([[[[6,7],[6,7]],[[0,7],[8,9]]],[[[[6,6],0],[6,16]],[[0,8],[8,0]]]], None)
+sollwert = Number([[[[6,7],[6,7]],[[0,7],[8,15]]],[[[0,6],[6,16]],[[0,8],[8,0]]]], None)
+#wrong result:    [[[[6,7],[6,7]],[[0,7],[8,9]]],[[[0,6],[6,16]],[[0,8],[8,0]]]]
+Number.debug = False
+t1.reduce()
+print(t1)
+
 
 
 
     
+    
+    
+    
 
-
-puzzle = read_puzzle('Tag_18_Bsp.txt')
-
-solve(puzzle)
---
+++ AoC2021/Tag_22.py
@@ -0,0 +1,157 @@
+from copy import deepcopy
+from collections import Counter, defaultdict
+import re
+from functools import reduce
+import operator #  flatten a lists with  reduce(operator.concat, l)
+
+class Cube:
+  def __init__(self, xmin, xmax, ymin, ymax, zmin, zmax) -> None:
+      assert xmin <= xmax
+      assert ymin <= ymax
+      assert zmin <= zmax
+
+      self.coords = [(xmin, xmax), (ymin, ymax) ,(zmin, zmax)]
+
+  def intersection(self, other):
+    test = [ (max(item[0]) < min(item[1])) or (min(item[0]) > max(item[1])) for item in  list(zip(self.coords, other.coords))]
+    has_intersection = not any(test)
+    if not has_intersection: return None
+    cube_coords = [(max(i[0][0], i[1][0]), min(i[0][1], i[1][1]))  for i in  list(zip(self.coords, other.coords))]
+    cube_coords = reduce(operator.concat, cube_coords) # Flatten the list
+    cube = Cube(*cube_coords)
+    return cube
+  
+  def getVolume(self):
+        v = 1
+        for c in self.coords:
+          dV = (c[1] - c[0] + 1)
+          assert dV >= 0
+          v *= dV
+        return v
+
+  def __str__(self) -> str:
+    s = ""
+    for c in self.coords:
+          s += "(" + str(c[0]) + ", " + str(c[1]) +") "
+    return s
+
+  def __repr__(self):
+    return self.__str__()
+
+def parse_cube(line:str, limitCoords):
+    swtich, line = line.split(" ")
+    coords = [int(i) for i in re.findall("-?\d+", line)]
+    if limitCoords and  ((min(coords) < -50) or (max(coords) > 50)):
+      c = None
+    else:
+      c = (swtich=="on", Cube(*coords))
+    return c
+
+def read_puzzle(datei, part1):
+  with open(datei) as f:
+    return [cube for cube in  [parse_cube(line.strip(), part1) for line in f.readlines()] if cube != None]
+
+''''
+on x=10..12,y=10..12,z=10..12
+off x=9..11,y=9..11,z=9..11
+'''
+
+def getTotalVolume(OnCubes,  R):
+  v = 0
+  for switch_cube, n in OnCubes.items():
+    v += n * R[switch_cube].getVolume()
+  return v
+
+def solve(switches_and_cubes):
+
+  OnCubes = Counter()
+  # es wurde "leider" eine eigene Klasse Cube erstellt.
+  # verschiedene Objekte mit gleichen Koordinaten sind verschiedene Objekte - und werden im Counter damit (exponentiell) mehrfach gezählt.
+  # daher wird als Schlüssel nicht das Objekt - sonder ein Hash - die Ausgabe von __str__()  - verwendet.
+  # die Regestry R dient als  "*Übersetzer" vom Hash zum Cube-Objekt
+  R = defaultdict()  # hash -> Cube (hash:= Cube.__str__())
+
+  for switch_cube in switches_and_cubes:
+    #print("\ncurrent cube:(swtich, coords, V)", switch_cube, switch_cube[1].getVolume())
+    current_switch, current_cube = switch_cube
+        
+    # ermittle alle Durchschnitte aller bisher bekannten "On"-Wurfel mit dem aktuellen Cube der Eingabe
+    # A = ist ein "ON" Cube aus dem Directory OnCubes (iterator), B ist aktueller Cube aus Input
+    # V(A + B)  = V(A) + V(B) - V(A intersecion B) - relevant, wenn B ist ein ON-cube
+    # V(A - B)  = V(A)        - V(A intersecion B) - relevant, wenn B ist in OFF-cube
+    onCubes_temp = Counter()
+    for onCube in OnCubes:
+      onCube = R[onCube]
+      intersection = onCube.intersection(current_cube)
+      if not intersection: continue
+      assert intersection.__str__() == current_cube.intersection(onCube).__str__()
+      onCubes_temp[intersection.__str__()] -= OnCubes[onCube.__str__()]
+      R[intersection.__str__()] = intersection
+      #print("Intersection found with:", onCube)
+      #print("Intersection=", intersection, " V=", intersection.getVolume(), " V_total:", getTotalVolume(OnCubes,  R))
+
+    # wenn aktueller Cube vom Type "on" -> Zaehle dieses Volumen EINMAL dazu
+    if current_switch:
+        onCubes_temp[current_cube.__str__()] += 1
+        R[current_cube.__str__()] = current_cube
+        #print("add curent cube to dict, V:", current_cube.getVolume(), " V_total:", getTotalVolume(OnCubes,  R))
+
+    for c in onCubes_temp:
+        OnCubes[c.__str__()] += onCubes_temp[c.__str__()]
+        R[current_cube.__str__()] = current_cube
+
+
+
+  return getTotalVolume(OnCubes, R)
+  
+def test():
+  coords_intersection = [5,9,-1,9,-12,3]
+
+  no_interssection = [
+    [4,5,-1,9,-12,3],
+    [5,9,-3,-1,-12,3],
+    [4,5,-1,9,-20,-12],
+    [9,10,-1,9,-12,3],
+    [5,9,9,10,3,4]
+  ]
+  c_i = Cube(*coords_intersection)
+  c_no_i = [Cube(*c) for c in no_interssection]
+
+  for c in c_no_i:
+    assert(c.intersection(c_i)  == None)
+
+  is_bigger = [
+      [4,9,-1,9,-12,3],
+      [5,9,-2,9,-12,3],
+      [5,9,-1,9,-13,3],
+      [5,10,-1,9,-12,3],
+      [5,9,-1,10,-12,3],
+      [5,9,-1,9,-12,4]
+    ]
+  c_no_i = [Cube(*c) for c in is_bigger]
+  for c in c_no_i:
+        assert(c.intersection(c_i).__str__()  == c_i.__str__())
+
+  is_lower = [
+
+  ]
+  
+
+      
+
+cubes = read_puzzle('Tag_22.txt', True)
+print("Part1:", solve(cubes))
+
+cubes = read_puzzle('Tag_22.txt', False)
+print("Parat2:",solve(cubes))
+
+#test()
+
+
+# for cube in cubes:
+#   print(cube[0], cube[1])
+
+
+
+
+
